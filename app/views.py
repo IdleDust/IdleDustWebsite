cf5863d26e26201a95df01569bf6034a2df5fe3c
JuanChenMacbookAir <JuanChen@JuanChendeMacBook-Air.local>
2016-09-15 17:07:32 -0700

    initial commit

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from app import app
+
+@app.route('/')
+def home():
+	return render_template("index.html")
+
+@app.route('/welcome')
+def welcome():
+    return render_template("welcome.html")
+
+@app.route('/about')
+def about():
+	return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+	return render_template("contact.html")
